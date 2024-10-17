69a129c17a8644ac8eae38ea2d086d5ead653dfe
FazliddinMirzaqosimov <fazliddinmirzaqosimov8@gmail.com>
2024-10-17 14:44:27 +0500

    bug fixed

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 const express = require("express");
 const axios = require("axios");
 const cors = require("cors");
- 
+
 const app = express();
-const port = 3000;
+const port = 9876;
 
 // Middleware to parse JSON bodies
 
 app.use(express.json());
 app.use(cors());
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 
 // Replace with your Telegram bot token and chat ID
 const telegramToken = "7815914215:AAFpLKyx0aKs4fB2lGsShzHVVg2-TrN1dv0";
-const chatId = "5520661044" || "-4506641503";
+const chatId = "-4506641503";
 
 // API endpoint to receive form data
 app.post("/send", async (req, res) => {
@@ -21,15 +21,13 @@ app.post("/send", async (req, res) => {
     // Extract form data from request body
     const data = req.body;
 
-     
     // Compose the message you want to send via Telegram
     let text = ``;
 
-    Object.keys(data)?.forEach(el => {
-      text =text + `${el}: ${data[el]}\n`
-    })
+    Object.keys(data)?.forEach((el) => {
+      text = text + `${el}: ${data[el]}\n`;
+    });
 
- 
     // Send the message to your Telegram bot using Axios
     await axios.post(
       `https://api.telegram.org/bot${telegramToken}/sendMessage`,
